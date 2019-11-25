3a75740b96fbc5874a26c8bbb492abf60f5efbb6
wyx0920 <924844920@qq.com>
2019-11-25 16:49:25 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
-
-
+import MainTabBar from './components/content/mainTabbar/MainTabBar.vue'
+import router from './router/index'
 export default {
   name: 'app',
   components: {
-  }
+    MainTabBar
+  },
+  router
 }
 </script>
 
