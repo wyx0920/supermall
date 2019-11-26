6e18c783ababcfffa961a903b6c9fb907f2988ba
wyx0920 <924844920@qq.com>
2019-11-26 17:55:09 +0800

    1

--- src/network/home.js
+++ src/network/home.js
@@ -5,3 +5,12 @@ export function getHomeMutidata(){
     url:'/home/multidata'
   })
 }
+export function getHomeGoods(type,page){
+  return request({
+    url:'/home/data',
+    params:{
+      type,
+      page
+    }
+  })
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,50 +4,11 @@
     <home-swiper :banners=banners></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
-    <tab-control :titles="['流行','新款','精选']"></tab-control>
+    <tab-control class="tab-control" 
+    :titles="['流行','新款','精选']" 
+    @tabClick="tabClick"></tab-control>
+    <goods-list :goods=goods[currentType].list></goods-list>
     
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-    </ul>
   </div>
 </template>
 <script>
@@ -57,9 +18,10 @@ import RecommendView from './childComps/RecommendView';
 import FeatureView from './childComps/FeatureView';
 
 import NavBar from 'components/common/navbar/NavBar';
-import TabControl from 'components/content/tabControl/TabControl'
+import TabControl from 'components/content/tabControl/TabControl';
+import GoodsList from 'components/content/goods/GoodsList';
 
-import {getHomeMutidata} from 'network/home.js';
+import {getHomeMutidata,getHomeGoods} from 'network/home.js';
 
 
 export default{
@@ -69,21 +31,62 @@ export default{
     HomeSwiper,
     RecommendView,
     FeatureView,
-    TabControl
+    TabControl,
+    GoodsList
   },
   data(){
     return{
       banners:[],
-      recommends:[]
+      recommends:[],
+      goods:{
+        'pop':{ page:0,list:[]},
+        'new':{ page:0,list:[]},
+        'sell':{ page:0,list:[]}
+      },
+      currentType:'pop'
     }
   },
   created(){
-    getHomeMutidata().then(res=>{
+   this.getHomeMutidata()
+   this.getHomeGoods('pop')
+   this.getHomeGoods('new')
+   this.getHomeGoods('sell')
+    
+  },
+  methods:{
+    /**
+     * 事件监听
+     */
+    tabClick(index){
+      switch(index){
+        case 0:this.currentType='pop'
+        break
+        case 1:this.currentType='new'
+        break
+        case 2:this.currentType='sell'
+        break
+      }
+      
+    },
+    /**
+     * 网络请求相关
+     */
+    getHomeMutidata(){
+       getHomeMutidata().then(res=>{
       // console.log(res)
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
-    })
+      })
+    },
+    getHomeGoods(type){
+      const page = this.goods[type].page+1
+      getHomeGoods(type,page).then(res=>{
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page+=1
+      })
+    }
   }
+
 }
 </script>
 <style scoped>
@@ -98,6 +101,11 @@ export default{
     left: 0;
     right: 0;
     top: 0;
+    z-index: 9;
+  }
+  .tab-control{
+    position: sticky;
+    top: 44px;
     z-index: 9;
   }
 </style>
